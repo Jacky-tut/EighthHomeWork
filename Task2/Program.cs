c9dc6798a02f3434847222685b8ede4827433f1b
Jacky_tut <netfinnov@mail.ru>
2022-12-27 22:46:51 +0300

    2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -9,3 +9,77 @@
 и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
 
+int GetNumber(string message)
+{
+    int result;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m,n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        matrix[i,j] = rnd.Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int GetMinSumString(int[,] matrix)
+{
+    int min = 0;
+    int minSum = 100;
+    int minString = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        min = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            min += matrix[i, j];
+        }
+        if (min < minSum)
+        {
+            minSum = min;
+            minString = i;
+        }
+    }
+    return minString + 1;
+}
+
+int numberRows = GetNumber("Введите количество строк");
+int numberColumns = GetNumber("Введите количество столбцов");
+int [,] matrix = InitMatrix(numberRows, numberColumns);
+PrintArray(matrix);
+Console.WriteLine($"Номер строки с наименьшей суммой элементов: {GetMinSumString(matrix)}");
+
