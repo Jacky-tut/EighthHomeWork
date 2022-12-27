e280f7d0590afcdada0c91b1158f38022c322253
Jacky_tut <netfinnov@mail.ru>
2022-12-27 23:37:34 +0300

    3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,3 +6,77 @@
 18 20
 15 18
 */
+
+int GetNumber(string message)
+{
+    int result;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m,n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        matrix[i,j] = rnd.Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] GetMatrixMultiplication(int[,] firstMatrix, int[,] secondMatrix)
+{
+    int[,] resultMatrix = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < firstMatrix.GetLength(1); k++)
+            {
+                resultMatrix[i,j] += firstMatrix[i, k] * secondMatrix[k, j];
+            }
+        }
+    }
+    return resultMatrix;
+}
+
+int numberRows = GetNumber("Введите количество строк первой матрицы");
+int numberColumns = GetNumber("Введите количество столбцов первой матрицы");
+int[,] firstMatrix = InitMatrix(numberRows, numberColumns);
+int numberSecondRows = GetNumber("Введите количество строк второй матрицы");
+int numberSecondColumns = GetNumber("Введите количество столбцов второй матрицы");
+int[,] secondMatrix = InitMatrix(numberSecondRows, numberSecondColumns);
+PrintArray(firstMatrix);
+PrintArray(secondMatrix);
+int [,] resultMatrix = GetMatrixMultiplication(firstMatrix, secondMatrix);
+PrintArray(resultMatrix);
+
